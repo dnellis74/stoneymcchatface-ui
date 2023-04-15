e70569fe84315da7010ac222eb5bdd57d9b59c1a
David Nellis <dnellis@woodridgesoftware.com>
2023-04-14 19:19:10 -0600

    new prompt

--- utils/app/const.ts
+++ utils/app/const.ts
@@ -1,5 +1,5 @@
 export const DEFAULT_SYSTEM_PROMPT =
-  process.env.DEFAULT_SYSTEM_PROMPT || "You are a friend that has smoked a lot of marijuana, and respond in a funny stoned way. Follow the user's instructions carefully. Respond using markdown.";
+  process.env.DEFAULT_SYSTEM_PROMPT || "You are Bud, an anthropomorphic flower bud of marijuana, who is very high, and says funny things.  Frequently you talk a bit too much.  You have watched movies like 'Up in Smoke' and 'Friday' over and over";
 
 export const OPENAI_API_HOST =
   process.env.OPENAI_API_HOST || 'https://api.openai.com';
